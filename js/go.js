9d6059ca781368a2f70a399210033ae857980b48
wjhh2008 <wjh2006-1@163.com>
2014-11-18 14:21:31 +0800

    add time check

--- js/go.js
+++ js/go.js
@@ -1,7 +1,24 @@
 
-function update(nu,value){
-    document.getElementById('s'+nu).innerHTML = value+'%';
-    document.getElementById('p'+nu).value = value
+function update(nu,value,time){
+    var txt = null;
+    var sp = document.getElementById('s'+nu);
+    var ps = document.getElementById('p'+nu);
+    
+    if (time>=3600000){
+        var h = Math.round(time/3600000);
+        txt = h>1?h+" hours ago":"an hour ago";
+        sp.style.color="red";
+    }else if(time>=60000){
+        var m = Math.round(time/60000);
+        txt = m>1?m+" minutes ago":"a minute ago";
+        if (m>15)
+            sp.style.color="#FFCC00";
+    }else {
+        txt = "a few seconds ago"
+    }
+    
+    sp.innerHTML = value+'%'+"    Last update "+txt;
+    ps.value = value;
 }
 function loadXML(){
     var xmlhttp = null;
@@ -14,22 +31,24 @@ function loadXML(){
         xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
     }
     
-    
-    
     //document.getElementById('test').innerHTML = "nice";
     xmlhttp.open("GET", "https://api.xively.com/v2/feeds/628656427.csv?timezone=+8", false);
     xmlhttp.setRequestHeader("X-ApiKey", "Wn6Cs6OuTACpKGlssPnOajl1sgsx0byLhgheUzl1MrI0tRbD");
     xmlhttp.send();
     if (xmlhttp.readyState==4 && xmlhttp.status==200){
         var lines = xmlhttp.responseText.split("\n");
+       // document.getElementById('test').innerHTML =null;
+       // document.getElementById('test1').innerHTML = ;
+        
         for (var i=0;i<lines.length-1;i++){
             var items = lines[i].split(",");
-            update(i,items[2]);
+            var passtime =Date.now()-new Date(items[1]).getTime();
+            update(i,items[2],passtime);
         }
-        document.getElementById('test').innerHTML = lines.length;
         //document.write(xmlhttp.responseText);
     }else{
         document.getElementById('test').innerHTML = xmlhttp.responseText;
     }
+    delete xmlhttp;
     
 }
